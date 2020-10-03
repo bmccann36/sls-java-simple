2ac90f9eab7bf3add81e9900fd1b20123f7c5dc7
mccann-b <brian@cedrus.digital>
2020-10-03 14:26:30 -0400

    fixes to /user and /users routes

--- src/main/java/com/serverless/GetUsersHandler.java
+++ src/main/java/com/serverless/GetUsersHandler.java
@@ -18,12 +18,11 @@ public class GetUsersHandler implements RequestHandler<Map<String, Object>, ApiG
         // make a map with some dummy data as response
         Map<String,Object> mockRes = new HashMap();
         mockRes.put("users", getMockUsers());
-        Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", null, mockRes);
 
         // return apiG responseHandler
         return ApiGatewayResponse.builder()
                 .setStatusCode(200)
-                .setObjectBody(responseBody)
+                .setObjectBody(mockRes)
                 .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
                 .build();
     }
